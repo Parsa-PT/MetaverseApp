0c8d0c8ce1c22a4af7797aa1f73e33d0985a41e4
Parsa-PT <mrparsashams@gmail.com>
2023-12-12 21:10:47 +0330

    first commit

--- components/StartStep.tsx
+++ components/StartStep.tsx
@@ -1,6 +1,11 @@
 import React from 'react'
 
-const StartStep = ({number , text}) => {
+type StartStepProp ={
+    number : number,
+    text:   string
+}
+
+const StartStep = ({number , text} : StartStepProp) => {
   return (
     <div className='flex justify-center items-center flex-row'>
       <div className=' flex justify-center items-center w-[70px] h-[70px]'></div>
